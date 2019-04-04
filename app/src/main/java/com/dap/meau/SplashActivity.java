d9f6a64c119735b926a862162b6a23bceb5575e3
LPignata <luanpignat@gmail.com>
2019-04-04 14:13:12 -0300

    Colocando SplashActivity em modo de tela cheia

--- app/src/main/java/com/dap/meau/SplashActivity.java
+++ app/src/main/java/com/dap/meau/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.dap.meau;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,15 +10,22 @@ import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final int DELAY_SECONDS = 1000; // Tempo de delay para mostrar a Splash Screen
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Altera a cor da barra de notificações
-        Window window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorAccent));
+        // Espera um tempo e continua
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Inicia a Main
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, DELAY_SECONDS);
     }
 }
