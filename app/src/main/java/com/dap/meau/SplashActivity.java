e7d63ba6658cc19225b468d80a7240e197edd36e
LPignata <luanpignat@gmail.com>
2019-04-04 13:56:16 -0300

    Adicionando tela inicial SplashActivity

---
+++ app/src/main/java/com/dap/meau/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.dap.meau;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Altera a cor da barra de notificações
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorAccent));
+    }
+}
