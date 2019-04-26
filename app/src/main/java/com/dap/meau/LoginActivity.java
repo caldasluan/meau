16c882b89a7d276e2c25352fa5f0eb09b607be24
LPignata <luanpignat@gmail.com>
2019-04-26 11:50:42 -0300

    Tela de login pronta

--- app/src/main/java/com/dap/meau/LoginActivity.java
+++ app/src/main/java/com/dap/meau/LoginActivity.java
@@ -2,6 +2,7 @@ package com.dap.meau;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Suporte para ActionBar
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle(getString(R.string.login));
+        setSupportActionBar(mToolbar);
     }
 }
