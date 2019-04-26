8789aec999adce65a5b25997ac0bbd672e603adb
LPignata <luanpignat@gmail.com>
2019-04-26 11:42:46 -0300

    Colocando scroll na tela de login e adicionando a tela para realizar login

--- app/src/main/java/com/dap/meau/MainActivity.java
+++ app/src/main/java/com/dap/meau/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dap.meau;
 
+import android.content.Intent;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -7,10 +8,13 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout mDrawerLayout;
+    Button mBtLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,18 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open_drawer, R.string.close_drawer);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        // Referência de Views
+        mBtLogin = findViewById(R.id.bt_main_login);
+
+        // Eventos de Click
+        mBtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
