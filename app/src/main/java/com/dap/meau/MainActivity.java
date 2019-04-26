ac1ed585bff74398abed224407e72ddef880fce7
Marcos Farias <20694062+fariasmarcos@users.noreply.github.com>
2019-04-26 12:48:50 -0300

    Remove bibliotecas sem uso na MainActivity.java
    Biblioteca sem uso não é de Deus. Adeus.

--- app/src/main/java/com/dap/meau/MainActivity.java
+++ app/src/main/java/com/dap/meau/MainActivity.java
@@ -3,11 +3,8 @@ package com.dap.meau;
 import android.content.Intent;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
