058c7b4071fac17c8a68a73323a73c791e6360d2
Luan Pignata <luanpignat@gmail.com>
2019-05-02 21:48:03 -0300

    Alterado nome da MainActivity

--- app/src/main/java/com/dap/meau/InitActivity.java
+++ app/src/main/java/com/dap/meau/InitActivity.java
@@ -1,23 +1,19 @@
 package com.dap.meau;
 
 import android.content.Intent;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.dap.meau.Util.MethodUtil;
-
-public class MainActivity extends AppCompatActivity {
+public class InitActivity extends AppCompatActivity {
 
     Button mBtLogin, mBtAdopt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_init);
 
         /*
         // Suporte para ActionBar e criação do Drawer
