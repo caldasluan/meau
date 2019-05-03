c351753b250d3e5368326980da449e8635837d3f
jvmunhoz <jvmunhoz@users.noreply.github.com>
2019-05-03 01:59:17 -0300

    Criar a Activity PerfilAnimal

---
+++ app/src/main/java/com/dap/meau/PerfilAnimal.java
@@ -0,0 +1,13 @@
+package com.dap.meau;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerfilAnimal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_animal);
+    }
+}
