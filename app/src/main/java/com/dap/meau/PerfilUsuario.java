2b34c1f9dd1b7932f0559429d0e4e3354ffc33a5
jvmunhoz <jvmunhoz@users.noreply.github.com>
2019-05-08 17:01:11 -0300

    Adicionar atividade PerfilUsuario no contexto do app
    Drawer foi atualizado.

--- app/src/main/java/com/dap/meau/PerfilUsuario.java
+++ app/src/main/java/com/dap/meau/PerfilUsuario.java
@@ -1,8 +1,10 @@
 package com.dap.meau;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class PerfilUsuario extends AppCompatActivity {
 
@@ -18,5 +20,19 @@ public class PerfilUsuario extends AppCompatActivity {
         mToolbar = findViewById(R.id.toolbar);
         mToolbar.setTitle(R.string.andre_santos);
         setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return false;
+        }
     }
 }
