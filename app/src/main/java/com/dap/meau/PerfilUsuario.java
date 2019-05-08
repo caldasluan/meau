8ca3d50a8b430f415323c537cc18280cfebdcada
jvmunhoz <jvmunhoz@users.noreply.github.com>
2019-05-08 11:49:01 -0300

    Adicionar nome do usuario na toolbar

--- app/src/main/java/com/dap/meau/PerfilUsuario.java
+++ app/src/main/java/com/dap/meau/PerfilUsuario.java
@@ -6,9 +6,17 @@ import android.support.v7.widget.Toolbar;
 
 public class PerfilUsuario extends AppCompatActivity {
 
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_usuario);
+
+
+        // Suporte para ActionBar
+        mToolbar = findViewById(R.id.perfil_usuario_toolbar);
+        mToolbar.setTitle("André Santos");
+        setSupportActionBar(mToolbar);
     }
 }
