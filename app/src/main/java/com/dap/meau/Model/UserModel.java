9e7d213efb5f1050e1d4b1f99096ae9539d3d79c
Marcos Farias <20694062+fariasmarcos@users.noreply.github.com>
2019-05-08 00:45:30 -0300

    Criar esqueleto da classe UserModel.java
    Define a classe e os atributos.

---
+++ app/src/main/java/com/dap/meau/Model/UserModel.java
@@ -0,0 +1,8 @@
+package com.dap.meau.Model;
+
+import java.io.Serializable;
+
+public class UserModel implements Serializable {
+    private String uid, shortName, fullName, email, state, city, address, username, password, phone, imageUrl, history;
+    private int age;
+}
