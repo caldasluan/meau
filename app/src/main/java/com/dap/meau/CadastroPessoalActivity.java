ac743d86135341a2e94e83627fbd69452e9332cd
jvmunhoz <jvmunhoz@users.noreply.github.com>
2019-05-10 01:41:02 -0300

    Remover userName e criar a variável shortName

--- app/src/main/java/com/dap/meau/CadastroPessoalActivity.java
+++ app/src/main/java/com/dap/meau/CadastroPessoalActivity.java
@@ -1,5 +1,7 @@
 package com.dap.meau;
 
+import java.io.*;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -15,7 +17,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 public class CadastroPessoalActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
-    TextView txtFullName, txtAge, txtEmail, txtEstate, txtCity, txtAdress, txtNumber, txtName, txtPass, txtConfirmPass;
+    TextView txtFullName, txtShortName, txtAge, txtEmail, txtEstate, txtCity, txtAdress, txtNumber, txtPass, txtConfirmPass;
     Button btSave, btImage;
     UserModel userModel;
 
@@ -32,6 +34,7 @@ public class CadastroPessoalActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         // Referência das Views
+        txtShortName = findViewById(R.id.perfil_usuario_txt_nome);
         txtFullName = findViewById(R.id.cad_pessoa_edit_nomecompleto);
         txtAge = findViewById(R.id.cad_pessoa_edit_idade);
         txtEmail = findViewById(R.id.cad_pessoa_edit_email);
@@ -39,7 +42,6 @@ public class CadastroPessoalActivity extends AppCompatActivity {
         txtCity = findViewById(R.id.cad_pessoa_edit_cidade);
         txtAdress = findViewById(R.id.cad_pessoa_edit_endereco);
         txtNumber = findViewById(R.id.cad_pessoa_edit_telefone);
-        txtName = findViewById(R.id.cad_pessoa_edit_nomeusuario);
         txtPass = findViewById(R.id.cad_pessoa_edit_senha);
         txtConfirmPass = findViewById(R.id.cad_pessoa_edit_confsenha);
         btSave = findViewById(R.id.cad_pessoa_btn_fazercadastro);
@@ -50,7 +52,6 @@ public class CadastroPessoalActivity extends AppCompatActivity {
         if (bundle != null && !bundle.isEmpty()) {
             userModel = (UserModel) bundle.getSerializable(UserModel.class.getName());
 
-            txtName.setText(userModel.getFullName());
             txtEmail.setText(userModel.getEmail());
         }
 
@@ -65,13 +66,13 @@ public class CadastroPessoalActivity extends AppCompatActivity {
 
     private void saveUser() {
         userModel.setFullName(txtFullName.getText().toString());
+        userModel.setShortName(txtFullName.getText().toString());
         userModel.setAge(Integer.valueOf(txtAge.getText().toString()));
         userModel.setEmail(txtEmail.getText().toString());
         userModel.setState(txtEstate.getText().toString());
         userModel.setCity(txtCity.getText().toString());
         userModel.setAddress(txtAdress.getText().toString());
         userModel.setPhone(txtNumber.getText().toString());
-        userModel.setShortName(txtName.getText().toString());
         userModel.setPassword(txtPass.getText().toString());
         UserDatabaseHelper.createUser(userModel, new OnSuccessListener() {
             @Override
