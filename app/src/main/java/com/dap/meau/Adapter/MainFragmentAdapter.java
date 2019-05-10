1eb9920c33eab385891499432643b0b5603df231
Luan Pignata <luanpignat@gmail.com>
2019-05-10 01:34:57 -0300

    Criar sistema para informar interesse em animal

--- app/src/main/java/com/dap/meau/Adapter/MainFragmentAdapter.java
+++ app/src/main/java/com/dap/meau/Adapter/MainFragmentAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,7 +39,7 @@ public class MainFragmentAdapter extends RecyclerView.Adapter<DefaultPetViewHold
             public void onClick(View view, int position) {
                 Intent intent = new Intent(mContext, PerfilAnimal.class);
                 Bundle bundle = new Bundle();
-                bundle.putSerializable(PetModel.class.getName(), mList.get(i));
+                bundle.putSerializable(PetModel.class.getName(), mList.get(position));
                 intent.putExtras(bundle);
                 mContext.startActivity(intent);
             }
